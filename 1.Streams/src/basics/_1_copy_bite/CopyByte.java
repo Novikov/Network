482efd5ce6a702ba30febba597d6105d3afe6c55
Igor Novikov <novikov9@yahoo.com>
2020-05-16 15:26:47 +0300

    stream basics

---
+++ 1.Streams/src/basics/_1_copy_bite/CopyByte.java
@@ -0,0 +1,30 @@
+package basics._1_copy_bite;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+//Копирование файла по одному байту. Очень дорогая операция. Частое обращение к диску.
+public class CopyByte {
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("1.jpeg");
+        OutputStream os = new FileOutputStream("2.jpeg");
+
+        //Данная функция читайет байты из потока, но возвращает число int,
+        // которое равняется числу прочитанных byte в виде 5 0 20 70 -1
+        //Мы ее используем как ключ, в цикле. Когда данная функция вернет -1 это будет означать, что мы достигли конца файла.
+        int r = is.read();
+
+        System.out.println(r);
+
+        while (r != -1){
+            os.write(r);
+            r = is.read();
+            System.out.println(r);
+        }
+
+        is.close();
+        os.close();
+    }
+}
---
+++ 1.Streams/src/basics/_2_coppy_array_bytes/CopyArrayBytes.java
@@ -0,0 +1,32 @@
+package basics._2_coppy_array_bytes;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+//Копирование файла c помощью массива байт - ускоряет эту операцию.
+//функция read() так же возвращает число прочитанных байт в виде -  4096б 4096 3831 -1.
+public class CopyArrayBytes {
+    public static void main(String[] args){
+        //Try с инициализацией ресурсов в скобках. Все ресурсы автоматически закрываются при выходе из try.
+        //Причем не важно словили мы исключение или нет.
+        try (InputStream is = new FileInputStream("1.jpeg");
+             OutputStream os = new FileOutputStream("3.jpeg");) {
+            byte [] buffer = new byte[4096];
+
+            int r = is.read(buffer);
+            System.out.println(r);
+            while (r != -1){
+                //os.write(r); данный метод не сработает т.к он пишет массив байт целиком, а он может быть заполненным не до конца.
+                // Поэтому нужно указывать конечную позицию
+                os.write(buffer,0,r);
+                r = is.read(buffer);
+                System.out.println(r);
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
